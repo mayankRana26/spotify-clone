2dd5eace666748912fda705c5462d1969d7c48fe
mayankRana26 <ranamayank987@gmail.com>
2025-05-26 11:42:00 +0530

    basic structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="bg-red-500">Hello</h1>
+    <Button>Click</Button>
+     </>
+  )
+}
+
+export default App
