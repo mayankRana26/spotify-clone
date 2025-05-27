4decc21b031e2eb5f57a003385a5ea113c052a44
mayankRana26 <ranamayank987@gmail.com>
2025-05-27 11:42:09 +0530

    Protect Route Middleware added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectRoute = async (req, res, next) => {
+    if(!req.auth.userId){
+        return res.status(401).json({mnessage:"Unauthorized- you should login first"});
+    }
+    next();
+};
+
+
+export const requireAdmin = async(req, res, next) => {
+    try {
+        const currentUser= await clerkClient.users.get(req.auth.userId);
+        const isAdmin=process.env.ADMIN_EMAIL ===currentUser.primaryEmailAddress?.emailAddress;
+
+        if(!isAdmin){
+            return res.status(401).json({mnessage:"Unauthorized- you should login first"});
+        }
+        next();
+    } catch (error) {
+        
+    }
+}
